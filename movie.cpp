a604494119a55cc96310c3d6d9525b746ab69edb
amin7942 <76994595+amin7942@users.noreply.github.com>
2023-05-09 18:40:48 +0900

    구조체 만드는중

---
+++ movie.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+struct Movie{
+    vector<string> name;
+    vector<string> movie_name;
+    vector<string> seat;
+    vector<int> reserve_seat;
+    vector<int> movie_price;
+    vector<string> date;
+    string movie_room;
+
+    Movie(){
+        
+    }
+};
